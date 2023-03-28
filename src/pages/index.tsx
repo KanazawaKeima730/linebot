c29c7c9c6f3b9d14228e495b457cdae8ae620220
KanazawaKeima730 <keima.kanazawa@bitbyte.co.jp>
2023-03-28 17:29:02 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
+import Head from "next/head";
 import type { NextPage } from "next";
-
+import { Header } from "../components/header";
+import { Hero } from "../components/Hero";
+import { Footer } from "../components/Footer";
 const Home: NextPage = () => {
   return (
-    <div>
-      <p>hello world</p>
-    </div>
+    <>
+      <Head>
+        <title>TaxPal - Accounting made simple for small businesses</title>
+        <meta
+          name="description"
+          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+      </main>
+      <Footer />
+    </>
   );
 };
 
